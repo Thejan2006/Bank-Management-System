4b6c2face852c4e9b341cf49b03eef3a99a0f520
Thejan2006 <hmagthejanbandar@gmail.com>
2026-08-11 18:28:07 +0530

    fix: update postcss.config.mjs to correct plugin configuration and export syntax

--- bank-frontend/postcss.config.mjs
+++ bank-frontend/postcss.config.mjs
@@ -1,13 +1,9 @@
+/** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
-
-export default config;
-module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
+
+export default config;
